aa084b18e4f347a086ecc069205edbc9e56944ec
rqbazan <saquirozbazan@gmail.com>
2017-11-07 17:53:39 +0000

    refactored files to create app

---
+++ app/puppercnn.py
@@ -0,0 +1,109 @@
+try:
+    import sys
+    import os
+    import cv2
+    import numpy as np
+    from glob import glob
+    from sklearn.datasets import load_files
+    from keras.utils import np_utils    
+    from keras.applications.resnet50 import ResNet50
+    from keras.models import Sequential
+    from keras.layers import GlobalAveragePooling2D, Dense
+    from keras.optimizers import Adamax
+    from keras.preprocessing import image    
+    from keras.applications.resnet50 import preprocess_input
+    from extract_bottleneck_features import *    
+except ImportError as e:
+    print(e)
+    sys.exit(1)
+
+ROOT_PATH = r'C:/Users/sciadmin/Documents/PupperCNN/Data'
+dog_train_data_directory = os.path.join(ROOT_PATH, "dogImages/train")
+
+with open("dog_names") as f:
+    dog_names = f.readlines()
+dog_names = [name.strip() for name in dog_names]
+
+face_cascade = cv2.CascadeClassifier('haarcascades/haarcascade_frontalface_alt.xml')
+print("face_cascade", face_cascade)
+
+"""
+    Iniciando el modelo de la ResNet50 alimentado por la base de conocimiento en image-net
+"""
+
+ResNet50_model = ResNet50(weights='imagenet')
+
+
+"""
+    Preparando las imágenes para procesarlas en TensorFlow
+"""
+
+def path_to_tensor(img_path):
+    img = image.load_img(img_path, target_size=(224, 224))
+    x = image.img_to_array(img)
+    return np.expand_dims(x, axis=0)
+
+def paths_to_tensor(img_paths):
+    list_of_tensors = [path_to_tensor(img_path) for img_path in img_paths]
+    return np.vstack(list_of_tensors)
+
+"""
+    Realizando la predicción con ResNet50
+"""
+
+def ResNet50_predict_labels(img_path):
+    img = preprocess_input(path_to_tensor(img_path))
+    return np.argmax(ResNet50_model.predict(img))
+
+def dog_detector(img_path):
+    prediction = ResNet50_predict_labels(img_path)
+    return ((prediction <= 268) & (prediction >= 151)) 
+
+def face_detector(img_path):
+    img = cv2.imread(img_path)
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray)
+    return len(faces) > 0
+
+bottleneck_features = np.load('bottleneck_features/DogResnet50Data.npz')
+train_ResNet50 = bottleneck_features['train']
+valid_ResNet50 = bottleneck_features['valid']
+test_ResNet50 = bottleneck_features['test']
+
+ResNet_model = Sequential()
+ResNet_model.add(GlobalAveragePooling2D(input_shape=train_ResNet50.shape[1:]))
+ResNet_model.add(Dense(133, activation='softmax'))
+
+ResNet_model.compile(loss='categorical_crossentropy', optimizer=Adamax(lr=0.002), metrics=['accuracy'])
+ResNet_model.load_weights('saved_models/weights.best_adamax.ResNet50.hdf5')
+
+def ResNet50_predict_breed(img_path):
+    bottleneck_feature = extract_Resnet50(path_to_tensor(img_path))
+    predicted_vector = ResNet_model.predict(bottleneck_feature)
+    breed = dog_names[np.argmax(predicted_vector)]
+    if dog_detector(img_path) == True:
+        return print("The breed of dog is a {}".format(breed))
+    else:
+        return print("If this person were a dog, the breed would be a {}".format(breed))
+
+def predict_breed(img_path):
+    isDog = dog_detector(img_path)
+    isPerson = face_detector(img_path)
+    if isDog:
+        print("Detected a dog")
+        breed = ResNet50_predict_breed(img_path)
+        return breed
+    if isPerson:
+        print("Detected a human face")
+        breed = ResNet50_predict_breed(img_path)
+        return breed
+    else:
+        print("No human face or dog detected")
+
+def main():
+    predict_breed('images/1.jpg')
+    predict_breed('images/2.jpg')
+
+
+if __name__ == '__main__':
+    main()
